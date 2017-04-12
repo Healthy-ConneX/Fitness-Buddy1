97c96d246e84625911c5bde5789dbdb59c95c640
Terri Byers <Terri Byers>
2017-04-12 19:56:50 -0400

    New Code

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,5 +1,12 @@
-
-
+// Initialize Firebase
+ var config = {
+   apiKey: "AIzaSyCcuXZXNmLFDO8EV2GrcK3XrorSbVOaC8I",
+   authDomain: "healthy-connex.firebaseapp.com",
+   databaseURL: "https://healthy-connex.firebaseio.com",
+   projectId: "healthy-connex",
+   storageBucket: "healthy-connex.appspot.com",
+   messagingSenderId: "376273698490"
+    };
 
    firebase.initializeApp(config);
    var database = firebase.database();
@@ -11,77 +18,11 @@
    var maleLLmap=[];
    var femMap=[];
    var femLLmap=[];
+   var allData = []; 
+   var allRun=[];
+   var allGym=[];
+   var allOut=[];
 
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyCcuXZXNmLFDO8EV2GrcK3XrorSbVOaC8I",
-    authDomain: "healthy-connex.firebaseapp.com",
-    databaseURL: "https://healthy-connex.firebaseio.com",
-    projectId: "healthy-connex",
-    storageBucket: "healthy-connex.appspot.com",
-    messagingSenderId: "376273698490"
-  };
-
-  firebase.initializeApp(config);
-
-  var database = firebase.database();
-
-$("#add-user-btn").on("click", function(event) {
-event.preventDefault();
-
-var userName = $("#full-name").val().trim();
-var addressOne = $("#address").val().trim();
-var city = $("#city").val().trim();
-var state = $("#state").val().trim();
-var zipCode = $("#postal-code").val().trim();
-var country = $("#country").val().trim();
-var mail = $("#email").val().trim();
-var password = $("#password").val().trim();
-var activity = $("#sel1").val().trim();
-var gender = $("#male").val().trim();
-var gender = $("#female").val().trim();
-var activity = $("#sel1").val().trim();
-var age = $("#sel2").val().trim();
-var skill = $("#sel3").val().trim();
-
-var newUser = {
-
-  name: userName,
- address: addressOne, 
- city: city,
- state: state,
- zip: zipCode,
- nation: country,
- email: mail,
- password: password,
- hobby: activity,
- gender: gender,
- age: age,
- level: skill
-
-};
-
-database.ref().push(newUser);
-
-console.log(newUser.name);
-
-
-
-
-
-
-
-
-
-
-
-
-
-});
-
-
-var password = document.getElementById("password")
-  , confirm_password = document.getElementById("confirm_password");
 
 var userName = $("")
 var addressOne = ("");
@@ -96,7 +37,6 @@ var gender = $("");
 var activity = $(""); //might have changed this around make sure its accurate
 var age = $("");
 var skill = $("");
-
 
 $("#add-user-btn").on("click", function(event) {
 event.preventDefault();
@@ -151,7 +91,9 @@ $("#sel2").val("");
 $("#sel3").val("");
 //Prevent moving to new page
   return false;
+
 });
+
 $("#search-user-btn").on("click", function(event) {
 
 event.preventDefault();
@@ -161,13 +103,23 @@ gender = $("#gender").val();
  age = $("#age").val();
 skill = $("#skill").val();
 
-var searchUser = {
-  gender: gender,
-  hobby: activity,
-  age: age,
-  level: skill
+for (var i = 0; i < allData.length; i++){
 
-};
+if (allData[i].hobby=== $("#interest").val()) {
+
+  $("#athlete-table > tbody").append("<tr><td>" + userName + "</td><td>" + activity + "</td><td>" + mail + "</td><td>" + gender + "</td></tr>"); 
+
+  console.log("whats up");
+        }
+      }
+ });
+
+$("#search-user-btn").on("click", function(event) {
+event.preventDefault();
+gender = $("#gender").val();
+ activity = $("#interest").val(); //might have changed this around make sure its accurate
+ age = $("#age").val();
+skill = $("#skill").val();
 
 
 
@@ -175,28 +127,20 @@ $("#gender").val(""); //changed these around maybe
 $("#interest").val("");
 $("#age").val("");
 $("#skill").val("");
-
  return false;
-
-
-
 });
-
-// $(document).ready(function () {
-//         $(".body").hide();
-//         $("#search-user-btn").click(function () {
-//             $(".body").show();
-//         });
-//       });
+$(document).ready(function () {
+        $(".body").hide();
+        $("#search-user-btn").click(function () {
+            $(".body").show();
+        });
+      });
 // hides the table's body on screen load and shows it on button
-
 //Create Firebase event for adding to the database and a 
 //row in the htmls when a user adds an entry
-
 //Create Firebase event for adding to the database and a 
 //row in the htmls when a user adds an entry
 database.ref().on("child_added", function(childSnapshot, prevChildKey){
-
 // console.log(childSnapshot.val());
 var userName = childSnapshot.val().name;
 //var addressOne = childSnapshot.val().address;
@@ -208,13 +152,14 @@ var coordinates=childSnapshot.val().coordinates;
 //changed this around 
 // console.log(userName);
 // console.log(activity);
+
+
  $("#athlete-table > tbody").append("<tr><td>" + userName + "</td><td>" + activity + "</td><td>" + mail + "</td><td>" + gender + "</td></tr>"); //changed this to add gender and mail
 });
 
 
 var maleAdd=[];
 var allAdd=[];
-
 database.ref().on("child_added",function(childSnapshot,prevChildKey){
   
     var streetAdd=childSnapshot.val().address;
@@ -226,9 +171,15 @@ database.ref().on("child_added",function(childSnapshot,prevChildKey){
     mapAddress.push(streetAdd+","+cityAdd + "," + stateAdd + "," + zipAdd);
     mapGender.push(gender);
     coords.push(coordinates);
+    allData.push(childSnapshot.val());
+
+ 
    // recAddress();
    mapDivide();
       });
+
+
+
 
 //make new arrays of male addresses per database and femail addresses per database
 function mapDivide(){
@@ -248,39 +199,31 @@ function mapDivide(){
       else if(mapGender[i]==="female"||mapGender[i]==="Female"){
         femMap.push(mapAddress[i]);
         femLLmap.push(mapAddress[i]);
-
       }
      }
-
     //  console.log(maleMap);
     // console.log(femMap);
      //console.log(maleLLmap);
      //console.log(femLLmap);
    
 }
-
-
-
 //initialize map function
 function initMap() {
         var map = new google.maps.Map(document.getElementById('map'), {
           zoom: 12,
-
           center: {lat: 35.913200, lng: -79.055847}
         });
         var geocoder = new google.maps.Geocoder();
         document.getElementById('submit').addEventListener('click', function() {
           geocodeAddress(geocoder, map);
         });
-
         setTimeout(function () {
           setMarkers();
         }, 500);
-
-    console.log("Map Initiated");
-      }
     
 
+      }
+    
 //place marker on map based on user input
 function geocodeAddress(geocoder, resultsMap) {
         var address = document.getElementById('address').value;
@@ -291,14 +234,10 @@ function geocodeAddress(geocoder, resultsMap) {
               map: resultsMap,
               position: results[0].geometry.location
             });
-
-            console.log("we are geocoding")
+          
           } 
         });
       }
-
-
-
 //put blue/pink pins on map to show where database members are located
    function setMarkers() {
     console.log('SET MARKERS');
@@ -312,7 +251,6 @@ function geocodeAddress(geocoder, resultsMap) {
         console.log(femMap);
         setMaleMarkers();
       };
-
  function setMaleMarkers() {
     console.log('SET MARKERS');
     var map = new google.maps.Map(document.getElementById('map'), {
@@ -340,7 +278,6 @@ function geocodeAddress(geocoder, resultsMap) {
         setFemMarkers();
       }//end for loop
     }
-
  function setFemMarkers() {
     console.log('SET MARKERS');
     var map = new google.maps.Map(document.getElementById('map'), {
@@ -360,10 +297,8 @@ function geocodeAddress(geocoder, resultsMap) {
             icon:"pinkBall.png",
             position: results[0].geometry.location
         });
-
         console.log(femMap);
         }
-
-      });
-    }
-  }
+      }); // end geocoder
+     } //end for loop
+    }//end setMarkers()
